494c35193c55e9a76173e66efb8975099418662a
Francisca <fvargasserani@gmail.com>
2020-12-27 18:16:14 -0300

    shortened js path

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,12 @@
 class PagesController < ApplicationController
+
     def index
-        Rails.logger.info("##########################################################################################")
-        Rails.logger.info("##########################################################################################")
-        Rails.logger.info("##########################################################################################")
-        Rails.logger.info("############Agrega un parrafo en la vista index.html.erb con el texto: Rails es genial!!!################")
-        Rails.logger.info("##########################################################################################")
-        Rails.logger.info("##########################################################################################")
-        Rails.logger.info("##########################################################################################")
     end
 
     def test
     end
-  
-end
 
-class UsersController < ApplicationController
-    def login
+    def about_us
     end
+
 end
